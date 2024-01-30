aae9b5d6eee6977e48af6f74aba81624e588a1b6
Alvin Betariza <alvin.b.alfred@gmail.com>
2024-01-30 14:40:36 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const yargs = require('yargs')
+
+const argv = yargs
+    .option('url', {
+        alias: 'u',
+        describe: 'URL to scrape',
+        demandOption: true,
+        type: 'string'
+    })
+    .option('selector', {
+        alias: 's',
+        describe: 'CSS selector to match the element',
+        demandOption: true,
+        type: 'string'
+    })
+    .help()
+    .argv
+
+async function scrapeWebPage(url,selector) {
+    try {
+        const response = await axios.get(url)
+        const html = response.data
+
+        const $ = cheerio.load(html)
+
+        const selectedElement = $(selector)
+
+        if(selectedElement.length === 0){
+            console.log('No element found with the provided selector')
+        }
+        else {
+            console.log('Text content of the selected element: ')
+            console.log(selectedElement.text())
+        }
+    } catch (err) {
+        console.error('Error: ', err.message)
+    }
+}
+
+scrapeWebPage(argv.url, argv.selector)
---
+++ index_2.js
@@ -0,0 +1,47 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function promptUser(){
+    return new Promise((res) => {
+        rl.question('Enter the URL to scrape: ', (url)=> {
+            rl.question('Enter the CSS selctor to match the element: ', (selector) => {
+                res({ url,selector })
+            })
+        })
+    })
+}
+
+async function scrapeWebPage(url,selector) {
+    try {
+        const response = await axios.get(url)
+        const html = response.data
+
+        const $ = cheerio.load(html)
+
+        const selectedElement = $(selector)
+
+        if(selectedElement.length === 0){
+            console.log('No element found with the provided selector')
+        }
+        else {
+            console.log('Text content of the selected element: ')
+            console.log(selectedElement.text())
+        }
+    } catch (err) {
+        console.error('Error: ', err.message)
+    }
+}
+
+async function main(){
+    const userInput = await promptUser()
+    scrapeWebPage(userInput.url, userInput.selector)
+    rl.close()
+}
+
+main()
